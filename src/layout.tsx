d28b5a2541ed1290313bcce0ef80789098debed2
Le-Duc-Nguyen-10 <nguyenldpd10357@gmail.com>
2025-10-11 21:29:36 +0700

    set absolute path and create directory structure

---
+++ src/layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+	return (
+		<>
+			<div>Hello world with nguyenld</div>
+			<Outlet />
+		</>
+	);
+}
+
+export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// react route setup
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "@/layout";
+import AboutPage from "pages/client/about";
+import BookPage from "pages/client/book";
+import HomePage from "pages/client/home";
+import LoginPage from "pages/client/auth/login";
+import RegisterPage from "pages/client/auth/register";
+
+import "styles/global.scss";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/home",
+				element: <HomePage />,
+			},
+			{
+				path: "/about",
+				element: <AboutPage />,
+			},
+			{
+				path: "/book",
+				element: <BookPage />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/register",
+		element: <RegisterPage />,
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		{/* <Layout /> */}
+		<RouterProvider router={router} />
+	</StrictMode>
+);
